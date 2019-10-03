9981a663abfe737705eed0fea2ad7e14409d4dd2
Jojo Zhang <jojozhang0327@gmail.com>
2019-10-03 14:33:16 -0400

    add post

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
+
+const shouts = [];
 
 server.get("/", (req, res) => {
   res.status(200).json({ hello: "Node 22" });
+});
+
+server.post("/shouts", (req, res) => {
+  shouts.push(req.body);
+
+  res.status(201).json(shouts);
 });
 
 // for heroku we need a dynamic port
